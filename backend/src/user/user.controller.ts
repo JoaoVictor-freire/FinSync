484753e62e7ce907aa193a2b23b85df6027051bd
jvfreire <joaovictorfreire.2003@gmail.com>
2026-03-16 15:46:21 -0300

    Feature/Backend/RescueUserBillsRoute

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,11 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service.js';
+import { JwtAuthGuard } from '../auth/jwt-auth.guards.js';
 
 @ApiTags('Usuários')
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService
+  ) {}
 
-
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({summary: "Resgata todas as contas de um usuário"})
+  @Get('mybills')
+  async findMyBills(@Request() req) {
+    return await this.userService.findUsersBills(req.user.sub);
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,15 +1,18 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { usuario } from '../../generated/prisma/browser.js';
+import { Injectable } from '@nestjs/common';
+import { usuario, contas } from '../../generated/prisma/browser.js';
 import { PrismaService } from '../prisma.service.js';
 
-
-export type User = usuario;
 
 @Injectable()
 export class UserService {
     constructor(
         private readonly prisma: PrismaService
     ) {}
+
+    async findUsersBills (idUsuario: string) : Promise<contas[]>{
+        const foundBills = await this.prisma.contas.findMany({
+            where: {idusuario: idUsuario}
+        })
+        return foundBills;
+    }
 }
-
-
